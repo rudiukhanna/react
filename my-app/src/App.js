f52ef987fb9e280362a54d5e66b9aa21af16482d
Rudiuk Hanna <melomanna@gmail.com>
2023-04-29 14:53:34 +0300

    styles files were added

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -2,6 +2,7 @@ import React from 'react';
 import Contacts from './Contacts';
 import Experience from './Experience';
 import Image from './Image';
+import '../src/styles.scss/styles.css'
 
 function App() {
   return <div>
