0fe395438ef62b539bf727d632d81f59918a35c2
Rudiuk Hanna <melomanna@gmail.com>
2023-05-02 15:38:21 +0300

    counter was added as new component

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,6 +1,7 @@
 import { Contacts } from './components/Contacts';
 import { Experience } from './components/Experience';
 import { Image } from './components/Image';
+import Counter from './components/Counter';
 import '../src/styles.scss/styles.css'
 
 const photoLink = './CVphoto.jpg';
@@ -10,6 +11,7 @@ function App() {
     <Image imageLink = {photoLink} />
     <Contacts/>
     <Experience/>
+    <Counter/>
   </div>
 }
 
