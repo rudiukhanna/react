e3bfcf0ebfd686105a6535945fe590d7c2ea59b4
Rudiuk Hanna <melomanna@gmail.com>
2023-05-03 12:16:51 +0300

    lesson_37

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -1,7 +1,6 @@
 import { Contacts } from './components/Contacts';
 import { Experience } from './components/Experience';
 import { Image } from './components/Image';
-import Counter from './components/Counter';
 import '../src/styles.scss/styles.css'
 
 const photoLink = './CVphoto.jpg';
@@ -11,7 +10,6 @@ function App() {
     <Image imageLink = {photoLink} />
     <Contacts/>
     <Experience/>
-    <Counter/>
   </div>
 }
 
