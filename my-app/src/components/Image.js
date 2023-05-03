28597f338dad3cc55b1f3e155cfc0f941c95b896
Rudiuk Hanna <melomanna@gmail.com>
2023-05-03 21:36:40 +0300

    <img> attributes width and height were changed

--- my-app/src/components/Image.js
+++ my-app/src/components/Image.js
@@ -1,6 +1,6 @@
 export function Image(props) {
     return (
-    <img src={props.imageLink} alt="My photo" width="290px" height="100%"/>
+    <img src={props.imageLink} alt="My photo" width="100%" height="290px"/>
     );
 }
 
